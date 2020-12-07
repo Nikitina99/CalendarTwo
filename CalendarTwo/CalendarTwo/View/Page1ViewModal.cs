ab955e8cf90b8f594827cbf73326af6923297fb7
Ульяна Никитина <iulianiy99@gmail.com>
2020-12-07 19:22:21 +0300

    Можно изменять начало месячеых. Изменен фон кнопок

--- CalendarTwo/CalendarTwo/View/Page1ViewModal.cs
+++ CalendarTwo/CalendarTwo/View/Page1ViewModal.cs
@@ -48,14 +48,45 @@ namespace CalendarTwo.View
         private async void Initialize()
         {
             var db = App.Database;
+            DateTime now = DateTime.Now;
+            DateTime dateS  = new DateTime(2020, 01, 1);
+
 
             //Дата начала месячных
             var table = await db.GetItemsAsync();
             var dat = new List<DateTime>();
+            var datMonth = new List<DateTime>();
+            var datNovemder = new List<DateTime>();
+
             foreach (var s in table)
-            {
-                dat.Add(s.Date);
+            {   if(s.Date > dateS)
+                {
+                    dat.Add(s.Date);
+                }
             }
+            
+                    var startDate = new DateTime(2020,10,1);
+                    var endDate = startDate.AddMonths(1).AddDays(-1);
+
+                    for (int b = dat.Count-1; b > 0; b--)
+                        {
+                            if (dat[b] > startDate && dat[b] < endDate)
+                            {
+                                datMonth.Add(dat[b]);
+                            }                        
+                        }
+
+            var startDateNovemder = new DateTime(2020, 11, 1);
+            var endDatestartDateNovemder = startDateNovemder.AddMonths(1).AddDays(-1);
+
+            for (int b = dat.Count-1; b > 0; b--)
+            {
+                if (dat[b] > startDateNovemder && dat[b] < endDatestartDateNovemder)
+                {
+                    datNovemder.Add(dat[b]);
+                }
+            }
+
 
             //Длительность мсч
             var count = new List<int?>();
@@ -80,9 +111,10 @@ namespace CalendarTwo.View
 
             var dates = new List<SpecialDate>();
             var num = count[count.Count - 1];
-            var lastday = dat[dat.Count-1];
+            var lastday = datMonth[0];
+           
 
-            //выводим сами мсч
+            //выводим сами мсч за октябрь
             for (int i=0 ; i<num; i++)
             {
                 var d = lastday.AddDays(i);
@@ -93,14 +125,40 @@ namespace CalendarTwo.View
                     BorderColor = Color.Fuchsia,
                     BorderWidth = 4,
                     Selectable = true
-                });                               
+                });
             }
 
+            bool first = false;
+            int index = 0;
+            //выводим мсч за ноябрь
+            foreach(var b in datNovemder)
+            {
+                if ((lastday - b).TotalDays <= -20 && first == false)
+                {
+                    lastday = b;
+                    //выводим сами мсч
+                    for (int i = 0; i < num; i++)
+                    {
+                        var d = lastday.AddDays(i);
+
+                        dates.Add(new SpecialDate(d)
+                        {
+                            BackgroundColor = Color.Pink,
+                            BorderColor = Color.Fuchsia,
+                            BorderWidth = 4,
+                            Selectable = true
+                        });
+                    }
+                    first = true;
+                }
+            } 
+                
+          
             //выводим прогноз на 3 месяца
             int j = 0;
             while (j < 3)
             {
-                DateTime d=new DateTime();
+               DateTime d = new DateTime();
                 var numPrognoz = period[period.Count - 1];
                 for (int i = 0; i < num; i++)
                 {
@@ -117,9 +175,7 @@ namespace CalendarTwo.View
                 Attendances = new ObservableCollection<SpecialDate>(dates);
                 j++;
                 lastday = d;
-            }
-            
-
+            }            
         }
     }
 }
